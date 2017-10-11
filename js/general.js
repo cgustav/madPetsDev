2577ea20806c38d86cae804058c35b7df1e5666d
Edward <gstv.qnd@outlook.com>
2017-10-11 11:04:47 -0300

    Actualizacion index
    slider mejorado y corrección a iconos de redes sociales. JS de control general integrado.

---
+++ js/general.js
@@ -0,0 +1,9 @@
+//JS para personalizar el slider del home de la pag. web
+JQuery (document).ready(function(){
+    JQuery('#slider-home').carousel({
+        interval:2000,
+        pause: "hover"
+    });
+}
+
+)
