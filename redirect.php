08ba2cd56a4826f4b48df8a2a93134ff00f9bd62
Edward <gstv.qnd@outlook.com>
2017-12-10 04:41:12 -0300

    trd chng dic
    client environment completion

--- redirect.php
+++ redirect.php
@@ -2,9 +2,9 @@
 session_start();
 if(isset($_SESSION['usuario'])){
 if($_SESSION['tipo']=="Administrador"){
-		header("Location: principal/admin/admin-dev.html");
+		header("Location: principal/admin/admin.php");
 	  }else if($_SESSION['tipo']=="Veterinario"){
-		header("Location: principal/veterinario/vet-dev.html");
+		header("Location: principal/veterinario/vet.php");
 	  }else if($_SESSION['tipo']=="Cliente"){
 	  header("Location: principal/cliente/cliente.php");
        }
